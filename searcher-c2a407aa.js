64c0020c159a71e06fefca57ac0396f4bcf27794
Jeetu-code <Jeetu-code@users.noreply.github.com>
2026-04-26 19:47:43 +0000

    deploy: 5099abdff577c28c4643bb3e35cdc1261d1bc921

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dc0f29fe.js',
+                path_to_root + 'searchindex-6966a12b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
